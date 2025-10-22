1b41ffaea20620225ddf5f1932cf814a66a9aac8
Ziad <62536773+ZiadMontaser@users.noreply.github.com>
2025-10-22 20:38:44 +0300

    Clean up main.dart by removing unused code
    Removed commented-out code related to Flutter app structure.

--- lam7a/lib/main.dart
+++ lam7a/lib/main.dart
@@ -65,26 +65,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-// import 'package:flutter/material.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'features/profile_summary/ui/view/profile_summary_page.dart';
-
-// void main() {
-//   runApp(const ProviderScope(child: MyApp()));
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Profile Summary Widget',
-//       theme: ThemeData(primarySwatch: Colors.blue),
-//       home: const ProfileSummaryPage(),
-//       routes: {
-//         '/profile': (_) => const Scaffold(body: Center(child: Text('Profile Page'))),
-//       },
-//     );
-//   }
-// }
