c3a2eae6631d79cb8f28eb777264d079e04450a5
ZiadMontaser <ziadmontaser2005@gmail.com>
2025-10-24 20:06:49 +0300

    fix(core): Change domain

--- lam7a/lib/core/constants/server_constant.dart
+++ lam7a/lib/core/constants/server_constant.dart
@@ -1,16 +1,15 @@
 
 class ServerConstant {
-
-    static String domain = "http://localhost:5000/api/v1.0";
-    static String registrationEndPoint = "/auth/register";
-    static String checkEmailEndPoint = "/auth/check-email";
-    static String verificationOTP = "/auth/verification-otp";
-    static String verifyOTP = "/auth/verify-otp";
-    static String resendOTP = "/auth/resend-otp";
-    static String login = "/auth/login";
-    static String logout = "/auth/logout";
-    static String refreshToken = "/auth/refresh-token";
-    static String  forgotPassword = "/auth/forgot-password";
-    static String resetPassword = "/auth/reset-password";
+  static String domain = "http://backend-code.duckdns.org/dev/api/v1.0";
+  static String registrationEndPoint = "/auth/register";
+  static String checkEmailEndPoint = "/auth/check-email";
+  static String verificationOTP = "/auth/verification-otp";
+  static String verifyOTP = "/auth/verify-otp";
+  static String resendOTP = "/auth/resend-otp";
+  static String login = "/auth/login";
+  static String logout = "/auth/logout";
+  static String refreshToken = "/auth/refresh-token";
+  static String  forgotPassword = "/auth/forgot-password";
+  static String resetPassword = "/auth/reset-password";
   static String serverURL = "http://backend-code.duckdns.org/dev";
 }
