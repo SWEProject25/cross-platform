64e27c132c4578aba33b2bc67c47adbb268e7b04
faroukmohamed2 <farmo6995@gmail.com>
2025-10-29 23:16:24 +0300

    fix(apiservice)

--- lam7a/lib/core/services/api_service.dart
+++ lam7a/lib/core/services/api_service.dart
@@ -6,7 +6,6 @@ import 'package:path/path.dart' as path;
 import 'package:path_provider/path_provider.dart';
 import 'dart:io';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
 // part 'api_service.g.dart';
 
 
