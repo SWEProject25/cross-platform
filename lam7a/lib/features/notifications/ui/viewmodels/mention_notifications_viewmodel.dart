05f20cbb8aaa9f09200cc15cfc7e12451bddc26f
Ziad <62536773+ZiadMontaser@users.noreply.github.com>
2025-12-07 23:46:14 +0200

    feat: Enhance notifications feature with new view models and pagination (#183)

---
+++ lam7a/lib/features/notifications/ui/viewmodels/mention_notifications_viewmodel.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lam7a/core/utils/logger.dart';
+import 'package:lam7a/features/common/providers/pagination_notifier.dart';
+import 'package:lam7a/features/common/states/pagination_state.dart';
+import 'package:lam7a/features/notifications/models/notification_model.dart';
+import 'package:lam7a/features/notifications/notifications_receiver.dart';
+import 'package:lam7a/features/notifications/proveriders/new_notifications_count.dart';
+import 'package:lam7a/features/notifications/repositories/notifications_repository.dart';
+import 'package:logger/logger.dart';
+
+final mentionNotificationsViewModelProvider =
+    NotifierProvider<
+      MentionNotificationsViewmodel,
+      PaginationState<NotificationModel>
+    >(() => MentionNotificationsViewmodel());
+
+class MentionNotificationsViewmodel
+    extends PaginationNotifier<NotificationModel> {
+  late final NotificationsRepository _notificationsRepository;
+  late NotificationsReceiver _notificationsReceiver;
+  final Logger _logger = getLogger(MentionNotificationsViewmodel);
+
+  @override
+  PaginationState<NotificationModel> build() {
+    _logger.i("Init MentionNotificationsViewmodel");
+    _notificationsRepository = ref.read(notificationsRepositoryProvider);
+    _notificationsReceiver = ref.read(notificationsReceiverProvider);
+
+    _notificationsRepository.markAllAsRead();
+
+    Future.microtask(() async {
+      try {
+        await loadInitial();
+      } catch (e) {
+        _logger.e("Error loading initial mention notifications: $e");
+      }
+    });
+
+    return const PaginationState<NotificationModel>();
+  }
+
+  @override
+  Future<(List<NotificationModel> data, bool hasMore)> fetchPage(
+    int page,
+  ) async {
+    try {
+      _logger.i("Fetching page $page of mention notifications");
+      return _notificationsRepository.fetchMentionNotifications(page, 20);
+    } catch (e) {
+      _logger.e("Error logging fetch page: $e");
+    }
+    return ([] as List<NotificationModel>, false);
+  }
+
+  @override
+  List<NotificationModel> mergeList(
+    List<NotificationModel> a,
+    List<NotificationModel> b,
+  ) {
+    return [...a, ...b];
+  }
+
+  void markAllAsRead() {
+    _logger.i("Marking All Mention notifications As Read");
+    _notificationsRepository.markAllAsRead();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      ref
+          .read(unReadNotificationCountProvider.notifier)
+          .updateNotificationsCount(reset: true);
+
+      var newState = state.copyWith(
+        items: state.items.map((n) => n.copyWith(isRead: true)).toList(),
+      );
+      state = newState;
+    });
+  }
+
+  void handleNotificationAction(NotificationModel notification) {
+    _notificationsReceiver.handleNotificationAction(notification);
+  }
+}
---
+++ lam7a/lib/features/notifications/ui/widgets/paginated_list.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lam7a/features/common/providers/pagination_notifier.dart';
+import 'package:lam7a/features/common/states/pagination_state.dart';
+
+class PaginatedListView<T extends PaginationNotifier<K>, K>
+    extends ConsumerStatefulWidget {
+  final NotifierProvider<T, PaginationState<K>> viewModelProvider;
+  final Widget Function(K item) builder;
+  final Widget noDataWidget;
+  final Widget endOfListWidget;
+
+  const PaginatedListView({
+    super.key,
+    required this.viewModelProvider,
+    required this.builder,
+    this.noDataWidget = const Center(child: Text("No Data Available")),
+    this.endOfListWidget = const Center(
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Text("No more items"),
+      ),
+    ),
+  });
+
+  @override
+  ConsumerState<PaginatedListView<T, K>> createState() =>
+      _PaginatedListViewState<T, K>();
+}
+
+class _PaginatedListViewState<T extends PaginationNotifier<K>, K>
+    extends ConsumerState<PaginatedListView<T, K>> {
+  late final ScrollController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = ScrollController();
+    _controller.addListener(_onScroll);
+  }
+
+  void _onScroll() {
+    if (_controller.position.pixels >=
+        _controller.position.maxScrollExtent - 200) {
+      ref.read(widget.viewModelProvider.notifier).loadMore();
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.removeListener(_onScroll);
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final state = ref.watch(widget.viewModelProvider);
+
+    return RefreshIndicator(
+      onRefresh: () {
+        if (state.isLoading || state.isRefreshing) {
+          return Future.value();
+        }
+        return ref.read(widget.viewModelProvider.notifier).refresh();
+      },
+      child: state.isLoading
+          ? Center(child: RefreshProgressIndicator())
+          : ListView.builder(
+              physics: const AlwaysScrollableScrollPhysics(),
+              controller: _controller,
+              itemCount: state.items.length + 1,
+              itemBuilder: (_, i) {
+                if (i == state.items.length) {
+                  if (state.items.isEmpty) {
+                    return SizedBox(
+                      height: MediaQuery.of(context).size.height - AppBar().preferredSize.height - MediaQuery.of(context).padding.top,
+                      child: widget.noDataWidget,
+                    );
+                  }
+
+                  if (state.isLoadingMore) {
+                    return Center(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: CircularProgressIndicator(),
+                      ),
+                    );
+                  }
+                  if (!state.hasMore) {
+                    return widget.endOfListWidget;
+                  }
+                }
+                return widget.builder(state.items[i]);
+              },
+            ),
+    );
+  }
+}
