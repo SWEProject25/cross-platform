8649901f0a5c71335bd6ac14a52b5e701fcda52d
HossamMo123 <mah24719782471978@gmail.com>
2025-12-15 17:55:59 +0200

    paging fix

--- lam7a/lib/features/add_tweet/ui/view/add_tweet_screen.dart
+++ lam7a/lib/features/add_tweet/ui/view/add_tweet_screen.dart
@@ -12,8 +12,12 @@ import 'package:lam7a/features/add_tweet/ui/widgets/add_tweet_header_widget.dart
 import 'package:lam7a/features/add_tweet/ui/widgets/add_tweet_toolbar_widget.dart';
 import 'package:lam7a/features/tweet/ui/viewmodel/tweet_viewmodel.dart';
 import 'package:lam7a/features/tweet/ui/widgets/tweet_body_summary_widget.dart';
-import 'package:lam7a/features/profile/ui/viewmodel/profile_posts_viewmodel.dart';
-import 'package:lam7a/features/profile/ui/viewmodel/profile_viewmodel.dart';
+// import 'package:lam7a/features/profile/ui/viewmodel/profile_posts_viewmodel.dart';
+//import 'package:lam7a/features/profile/ui/viewmodel/profile_viewmodel.dart';
+import 'package:lam7a/features/profile/ui/viewmodel/profile_likes_pagination.dart';
+import 'package:lam7a/features/profile/ui/viewmodel/profile_replies_pagination.dart';
+import 'package:lam7a/features/profile/ui/viewmodel/profile_posts_pagination.dart';
+
 import 'dart:io';
 import 'package:permission_handler/permission_handler.dart';
 
